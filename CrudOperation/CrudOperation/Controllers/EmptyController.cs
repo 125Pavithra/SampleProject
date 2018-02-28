624d1f4a093a706aacc85c0405c461f29bc8080a
125Pavithra <125pavithra@gmail.com>
2018-02-28 14:28:23 +0530

    CommitV1

---
+++ CrudOperation/CrudOperation/Controllers/EmptyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CrudOperation.Controllers
+{
+    public class EmptyController : ApiController
+    {
+        public string Post()
+        {
+            return "Hello World";
+        }
+    }
+}
